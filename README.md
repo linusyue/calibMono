e8cea3d56a5cbbbada7d620ea1690539500632e9
linusyue <93686722@qq.com>
2019-10-20 16:16:28 +0800

    Add dataset

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ Make sure your are in the `build` folder to run the executables.
 
 ### Running
 ```bash
-./calibMono -w=9 -h=6 -s=0.02423 -o=camera.yml -op -oe imagelist.xml
+./calibMono -w=9 -h=6 -s=0.02423 -o=camera.yml -op -oe -su imagelist.xml
 
 results saved in camera.yml
---
+++ build/CMakeFiles/calibMono.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibMono.dir/calibration.cpp.o"
+  "calibMono.pdb"
+  "calibMono"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibMono.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
